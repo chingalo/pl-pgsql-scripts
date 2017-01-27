632ec70c54ab7610043721c747669063f5e9d5dd
Joseph Chingalo <profschingalo@gmail.com>
2017-01-27 15:56:40 +0300

    fiter orgunit level greater than 3

--- orgUnitCodesGenerator.sql
+++ orgUnitCodesGenerator.sql
@@ -1,17 +1,22 @@
 DROP FUNCTION organisationUnitsCodeGenerator();
 DROP FUNCTION getOrganisationUnitsbyLevel();
 DROP FUNCTION getParentOrganisationUnitCode(INT);
+DROP FUNCTION updateOrganisationUnitCode(INT,VARCHAR);
 DROP TYPE holder;
 
---Village level = 5, water pint level = 6
+-- ward level = 4, Village level = 5, water pint level = 6
 
 --holder for some properties of orgunits 
 CREATE  TYPE holder AS (organisationunitid INT,uid VARCHAR(11),name VARCHAR(50),code VARCHAR(50),hierarchylevel INT,parentid INT);
 
--- function to get all ordered by orgunits
-CREATE OR REPLACE FUNCTION  getOrganisationUnitsbyLevel() RETURNS SETOF holder AS 'SELECT organisationunitid,uid,name,code,hierarchylevel,parentid FROM organisationunit ORDER BY created ASC;' language 'sql';
+-- function to get all ordered by orgunits with level greater then 3
+CREATE OR REPLACE FUNCTION  getOrganisationUnitsbyLevel() RETURNS SETOF holder AS 
 
---replace loops wijht appropriate function
+	'SELECT organisationunitid,uid,name,code,hierarchylevel,parentid FROM organisationunit WHERE hierarchylevel > 3 ORDER BY created ASC;' 
+	language 'sql';
+	
+
+--replace loops with appropriate function
 CREATE OR REPLACE FUNCTION getParentOrganisationUnitCode(parentid INT) RETURNS TEXT AS $$
 	DECLARE
 		organisationUnit holder%ROWTYPE;
@@ -26,27 +31,32 @@ CREATE OR REPLACE FUNCTION getParentOrganisationUnitCode(parentid INT) RETURNS T
 	END;
 	$$ LANGUAGE plpgsql; 
 
+
 -- function for code generations
 CREATE OR REPLACE FUNCTION organisationUnitsCodeGenerator() RETURNS VOID AS $$
 	DECLARE
 		--variable to holder types
 		organisationUnit holder%ROWTYPE;
 		waterPoint holder%ROWTYPE;
-		villageLevel INT :=2;
+		wardLevel INT :=4;
+		villageLevel INT :=5;
 		counter INT:= 0;
 		code VARCHAR(50) :='';
 		parentCode VARCHAR(50);
 	BEGIN
+		-- fix for testing parentid = 4096
 		FOR organisationUnit IN SELECT * FROM getOrganisationUnitsbyLevel() LOOP			
 			parentCode := getParentOrganisationUnitCode(organisationUnit.parentid);
 			IF  organisationUnit.hierarchylevel = villageLevel THEN
 				RAISE NOTICE '**************************************************************************************************';	
 				RAISE NOTICE '****************** Starting code genration for  % village *******************',organisationUnit.name;
 				RAISE NOTICE '**************************************************************************************************';
+				--checking for code uniqueness
 				code := upper(substr(organisationUnit.name,0,4));
 				code := CONCAT(CONCAT(parentCode,'.'),code);
-				RAISE NOTICE 'Code for % is % ',organisationUnit.name,code;
+				RAISE NOTICE 'Code for % is % ',organisationUnit.name,code; 
 				--update codes 
+				
 				RAISE NOTICE '==== codes generation water points in  % villages=======',organisationUnit.name;
 				counter := 0;
 				FOR waterPoint IN SELECT * FROM getOrganisationUnitsbyLevel() WHERE parentid= organisationUnit.organisationunitid LOOP
@@ -64,10 +74,12 @@ CREATE OR REPLACE FUNCTION organisationUnitsCodeGenerator() RETURNS VOID AS $$
 				END LOOP;
 				RAISE NOTICE '**************************************************************************************************';	
 				RAISE NOTICE '******************* End code genration for  % village ******************',organisationUnit.name;
-				RAISE NOTICE '**************************************************************************************************';
-				RAISE NOTICE'';
-				RAISE NOTICE '';
+				RAISE NOTICE '**************************************************************************************************';				
+			ELSE
+			RAISE NOTICE '******************skip code generation for % ***********',organisationUnit.name;
+			RAISE NOTICE'';
 			END IF;	
+			
 		END LOOP;				
 	END;
 	$$ LANGUAGE plpgsql; 	

b7d183427223431cbcb97b469ecd60ab1e8a0525
Joseph Chingalo <profschingalo@gmail.com>
2017-02-08 17:05:26 +0300

    rename functions to understandable words

--- villagesAndWaterPointsCodesGenerator.sql
+++ villagesAndWaterPointsCodesGenerator.sql
@@ -1,9 +1,8 @@
-DROP FUNCTION organisationUnitsCodeGenerator();
-DROP FUNCTION getOrganisationUnitsbyLevel();
-DROP FUNCTION getParentOrganisationUnitCode(INT);
+DROP FUNCTION villagesAndWaterPointsCodeGenerator();
+DROP FUNCTION getOrganisationUnits();
 DROP FUNCTION updateOrganisationUnitCodes(INT,VARCHAR);
 DROP FUNCTION getVillageCode(INT,INT,INT,VARCHAR,VARCHAR);
-DROP FUNCTION generateVillageCode(INT,INT,INT,VARCHAR,VARCHAR);
+DROP FUNCTION getAndUpdateUniqueVillageCode(INT,INT,INT,VARCHAR,VARCHAR,INT);
 DROP TYPE holder;
 
 -- ward level = 4, Village level = 5, water pint level = 6
@@ -12,39 +11,24 @@ DROP TYPE holder;
 CREATE  TYPE holder AS (organisationunitid INT,uid VARCHAR(11),name VARCHAR(50),code VARCHAR(50),hierarchylevel INT,parentid INT);
 
 -- function to get all ordered by orgunits created dates
-CREATE OR REPLACE FUNCTION  getOrganisationUnitsbyLevel() RETURNS SETOF holder AS 
-
+CREATE OR REPLACE FUNCTION  getOrganisationUnits() RETURNS SETOF holder AS 
 	'SELECT organisationunitid,uid,name,code,hierarchylevel,parentid FROM organisationunit ORDER BY created ASC;' 
 	language 'sql';
 	
 
---function to update codes
+
+--function to update code for a given organisation unit
 CREATE OR REPLACE FUNCTION updateOrganisationUnitCodes(orgunitId INT, newCode VARCHAR) RETURNS VOID AS
 $$
 BEGIN
-    UPDATE organisationunit  SET code = orgunitId WHERE organisationunitid = orgunitId; --AND code = '';
+    UPDATE organisationunit  SET code = newCode WHERE organisationunitid = orgunitId; --AND code = '';
     EXCEPTION WHEN  unique_violation THEN
-		RAISE NOTICE 'vaolate uniqueness on orgunit with % and code %',orgunitId,newCode;
-		--UPDATE organisationunit  SET code = newCode WHERE organisationunitid = orgunitId;
+		newCode := newCode;
+		RAISE NOTICE 'unique_violation on orgunit with % and code %',orgunitId,newCode;
 END;
 $$
 LANGUAGE plpgsql;
 
-
---replace loops with appropriate function
-CREATE OR REPLACE FUNCTION getParentOrganisationUnitCode(parentid INT) RETURNS TEXT AS $$
-	DECLARE
-		organisationUnit holder%ROWTYPE;
-		code TEXT := '';
-	BEGIN		
-		FOR organisationUnit IN SELECT * FROM getOrganisationUnitsbyLevel() LOOP
-			IF organisationUnit.organisationunitid=parentid  THEN
-				code := organisationUnit.code;
-			END IF;
-		END LOOP;		
-		RETURN code;
-	END;
-	$$ LANGUAGE plpgsql; 
 
 --generate codes based on postion of characters
 CREATE OR REPLACE FUNCTION getVillageCode(firstLetterPosition INT,secondLetterPosition INT,thirdLetterPosition INT,villageName VARCHAR,parentCode VARCHAR) RETURNS VARCHAR AS
@@ -58,14 +42,15 @@ END;
 $$
 LANGUAGE plpgsql;
 
+
 --function to get unique village code 
-CREATE OR REPLACE FUNCTION generateVillageCode(firstLetterPosition INT,secondLetterPosition INT,thirdLetterPosition INT,villageName VARCHAR,parentCode VARCHAR) RETURNS VARCHAR AS
+CREATE OR REPLACE FUNCTION getAndUpdateUniqueVillageCode(firstLetterPosition INT,secondLetterPosition INT,thirdLetterPosition INT,villageName VARCHAR,parentCode VARCHAR,orgunitId INT) RETURNS VARCHAR AS
 $$
 DECLARE
 	villageCode VARCHAR := '';
 BEGIN	
 	villageCode :=getVillageCode(firstLetterPosition,secondLetterPosition,thirdLetterPosition,villageName,parentCode);
-	IF ((SELECT COUNT(code) FROM getOrganisationUnitsbyLevel() WHERE code = villageCode ) > 0) THEN
+	IF ((SELECT COUNT(code) FROM getOrganisationUnits() WHERE code = villageCode ) > 0) THEN
 		thirdLetterPosition := thirdLetterPosition + 1;
 		IF(thirdLetterPosition >= char_length(villageName)) THEN 
 			secondLetterPosition := secondLetterPosition + 1;
@@ -77,6 +62,7 @@ BEGIN
 		END IF;
 		RETURN getVillageCode(firstLetterPosition,secondLetterPosition,thirdLetterPosition,villageName,parentCode);
 	ELSE 
+		PERFORM updateOrganisationUnitCodes(orgunitId,villageCode);
 		RETURN villageCode;
 	END IF;	
 	
@@ -84,11 +70,12 @@ END;
 $$
 LANGUAGE plpgsql;
 
+
 -- function for code generations
-CREATE OR REPLACE FUNCTION organisationUnitsCodeGenerator() RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION villagesAndWaterPointsCodeGenerator() RETURNS VOID AS $$
 	DECLARE
 		--variable to holder types
-		organisationUnit holder%ROWTYPE;
+		ward holder%ROWTYPE;
 		village holder%ROWTYPE;
 		waterPoint holder%ROWTYPE;
 		districtLevel INT :=3;
@@ -99,33 +86,32 @@ CREATE OR REPLACE FUNCTION organisationUnitsCodeGenerator() RETURNS VOID AS $$
 		newCode VARCHAR(50) :='';
 		parentCode VARCHAR(50);
 	BEGIN
-		-- fix values  for testing parentid = 4096 parentid = 4096 LOOP--
+		-- fix values  for testing parentid = 4096 LOOP--
 		
 		--codes for villages
 		RAISE INFO '::::::::::::::::::::::::::::::::::::::::';
 		RAISE INFO ':::::::: Villages codes :::::::::::::::::';
 		RAISE INFO ':::::::::::::::::::::::::::::::::::::::::';
-		FOR organisationUnit IN SELECT * FROM getOrganisationUnitsbyLevel() WHERE hierarchylevel = wardLevel LOOP			
-			--RAISE INFO 'Starting code generation for % ward ',organisationUnit.name;
-			FOR village IN SELECT * FROM getOrganisationUnitsbyLevel() WHERE parentid = organisationUnit.organisationunitid LOOP			
-				parentCode := organisationUnit.code;
+		FOR ward IN SELECT * FROM getOrganisationUnits() WHERE hierarchylevel = wardLevel LOOP
+			FOR village IN SELECT * FROM getOrganisationUnits() WHERE parentid = ward.organisationunitid LOOP			
+				parentCode := ward.code;
 				orgunitId := village.organisationunitid;
-				newCode := generateVillageCode(1,2,3,village.name,parentCode);				
-				raise info 'village %  ::: code %  ::  parentcode %',village.name,newCode,parentCode;
+				newCode := getAndUpdateUniqueVillageCode(1,2,3,village.name,parentCode,orgunitId);				
+				RAISE INFO 'village %  ::: code %  ::  parentcode %',village.name,newCode,parentCode;
 				--update codes 				
-				PERFORM updateOrganisationUnitCodes(orgunitId,newCode);				
+				--PERFORM updateOrganisationUnitCodes(orgunitId,newCode);				
 			END LOOP;
-		
-			--RAISE INFO 'Ending code generation for % ward ',organisationUnit.name;			
 		END LOOP;
+		
+		--water points codesrtions
 		RAISE INFO '::::::::::::::::::::::::::::::::::::::::';
 		RAISE INFO ':::::::: Water points codes:::::::::::::;';
-		RAISE INFO '::::::::::::::::::::::::::::::::::::::::';
-		--water points codesrtions
-		FOR village IN SELECT * FROM getOrganisationUnitsbyLevel() WHERE hierarchylevel = villageLevel LOOP			
+		RAISE INFO '::::::::::::::::::::::::::::::::::::::::';		
+		FOR village IN SELECT * FROM getOrganisationUnits() WHERE hierarchylevel = villageLevel LOOP			
 			counter := 0;
 			--update codes for villages
-			FOR waterPoint IN SELECT * FROM getOrganisationUnitsbyLevel() WHERE parentid = village.organisationunitid LOOP
+			RAISE INFO 'water points in  %  village',village.name;
+			FOR waterPoint IN SELECT * FROM getOrganisationUnits() WHERE parentid = village.organisationunitid LOOP
 				orgunitId = waterPoint.organisationunitid;
 				counter := counter + 1;
 				parentCode := village.code;
@@ -147,5 +133,5 @@ CREATE OR REPLACE FUNCTION organisationUnitsCodeGenerator() RETURNS VOID AS $$
 	END;
 	$$ LANGUAGE plpgsql; 	
 
---- gerate codes
-SELECT organisationUnitsCodeGenerator();
+--- intiate code generation for villages and water points
+SELECT villagesAndWaterPointsCodeGenerator();

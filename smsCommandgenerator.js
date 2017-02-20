67988f1850258424b03ffd113761cd82d0e5ac45
Joseph Chingalo <profschingalo@gmail.com>
2017-02-20 12:25:27 +0300

    fixing some issues on sms commnads generations

--- smsCommandgenerator.js
+++ smsCommandgenerator.js
@@ -2,56 +2,49 @@ prepareSmsCode();
 function prepareSmsCode (){
 	var DHIS2URL = "http://localhost:8080/dhis/api/dataSets.json";
 	var formCode = "HMIS";
-	var smsCommndsObject = [];
-	var smsCommandDefault = {
-		  "commandName": "",
-		  "parserType": "KEY_VALUE_PARSER",
-		  "separator": ":",
-		  "smsCode": []
-		}
-		var smsCodeDefault = {
-		  "smsCode": formCode,
-		  "dataElement": {
-			"name": "",
-			"created": "",
-			"lastUpdated": "",
-			"translations": [],
-			"externalAccess": false,
-			"displayName": "",
-			"id": ""
-		  },
-		  "compulsory": false,
-		  "categoryOptionCombos":""
-		};
  $.ajax({
 		url: DHIS2URL ,
 		type: 'GET',
 		data:'paging=false&filter=name:ilike:HMIS&fields=id,name,dataSetElements[dataElement[id,name,translations,lastUpdated,created,externalAccess,displayName,categoryCombo[categoryOptionCombos[id,name]]]]',
 	}).done( function(response) {
-		response.dataSets.forEach(function(dataSet,dataSetIndex){
-			var smsCommand = smsCommandDefault;
-			smsCommand.commandName = dataSet.id;
-			dataSet.dataSetElements.forEach(function(dataSetElement,dataSetElementIndex){
-				var dataElement = dataSetElement.dataElement;
-				dataElement.categoryCombo.categoryOptionCombos.forEach(function(categoryOptionCombos,categoryOptionCombosIndex){
-					var smsCode = smsCodeDefault;
-					smsCode.smsCode = smsCode.smsCode+dataSetIndex+categoryOptionCombosIndex;
-					smsCode.dataElement.name = dataElement.name;
-					smsCode.dataElement.created = dataElement.created;
-					smsCode.dataElement.lastUpdated = dataElement.lastUpdated;
-					smsCode.dataElement.translations = dataElement.translations;
-					smsCode.dataElement.externalAccess = dataElement.externalAccess;
-					smsCode.dataElement.displayName = dataElement.displayName;
-					smsCode.dataElement.id = dataElement.id;
-					smsCode.dataElement.categoryOptionCombos = categoryOptionCombos.id;
+		var dataSets = response.dataSets;		
+		for(dataSetIndex = 0;dataSetIndex < dataSets.length; dataSetIndex++ ){
+			var secondValue = 0;
+			var dataSet = dataSets[dataSetIndex];
+			var smsCommand = {
+			  "commandName": dataSet.id,
+			  "parserType": "KEY_VALUE_PARSER",
+			  "separator": ":",
+			  "smsCode": []
+			};
+			var dataSetElements = dataSet.dataSetElements;				
+			for(dataSetElementIndex = 0;dataSetElementIndex < dataSetElements.length; dataSetElementIndex++ ){				
+				var dataElement = dataSetElements[dataSetElementIndex].dataElement;
+				for(categoryOptionCombosIndex = 0;categoryOptionCombosIndex < dataElement.categoryCombo.categoryOptionCombos.length; categoryOptionCombosIndex++ ){
+					var categoryOptionCombos = dataElement.categoryCombo.categoryOptionCombos[categoryOptionCombosIndex];
+					secondValue ++;
+					var smsCode = {
+					  "smsCode": formCode+dataSetIndex+secondValue,
+					  "dataElement": {
+						"name": dataElement.name,
+						"created": dataElement.created,
+						"lastUpdated": dataElement.lastUpdate,
+						"translations": dataElement.translations,
+						"externalAccess": false,
+						"displayName": dataElement.displayNa,
+						"id": dataElement.id
+					  },
+					  "compulsory": false,
+					  "categoryOptionCombos":categoryOptionCombos.id
+					};
 					//push sms code into given sms command
-					smsCommand.push(smsCode);
-				});
-			});
-			//smscomand to overall sms command object
-			smsCommndsObject.push(smsCommand);
-			console.log("=========================");
-		});
-		console.log(smsCommndsObject);
+					smsCommand.smsCode.push(smsCode);
+				}
+			}
+			
+			console.log("");
+			console.log(JSON.stringify(smsCommand))
+		}
+		
 	});
 }

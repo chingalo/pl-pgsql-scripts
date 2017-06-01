f8efc2fb3260aa5461e552d2e4f4de5848f6ce4b
Joseph Chingalo <profschingalo@gmail.com>
2017-06-01 10:08:59 +0300

    save data using parameter

--- organisation-unit-app/organsaitionUnitApp.sql
+++ organisation-unit-app/organsaitionUnitApp.sql
@@ -1,13 +1,12 @@
 DROP FUNCTION deleteOrganisationUnit(VARCHAR);
 
-CREATE OR REPLACE FUNCTION deleteOrganisationUnit(orgUnitId VARCHAR) RETURNS VOID AS $$
-$$
+CREATE OR REPLACE FUNCTION deleteOrganisationUnit(orgunit VARCHAR) RETURNS VOID AS $$
 DECLARE
+	organsationUnit RECORD;
 
 BEGIN
-	
-	
-	RAISE INFO 'orgUnitId to be delete is ',orgUnitId;	
+	RAISE INFO 'orgUnitId to be delete is %',orgunit;
+	EXECUTE format('COPY((SELECT * FROM organisationunit WHERE uid = %L)) TO %L WITH CSV HEADER;',orgunit,CONCAT(CONCAT('/home/chingalo/development/scripts/organisation-unit-app/data/orgunit_',orgunit),'.csv'));	
 END;
 $$
 LANGUAGE plpgsql;
@@ -15,5 +14,6 @@ LANGUAGE plpgsql;
 /*
  call delete function by pass orgunit id
 */
-SELECT deleteOrganisationUnit('joseph');
+SELECT deleteOrganisationUnit('CAWjYmd5Dea');
+
 

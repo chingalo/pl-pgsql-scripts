ebd3617da895d410b1c01163797c50c7468a9db7
Joseph Chingalo <profschingalo@gmail.com>
2017-06-01 10:50:26 +0300

    backup data with ilike functions

--- organisation-unit-app/organsaitionUnitApp.sql
+++ organisation-unit-app/organsaitionUnitApp.sql
@@ -2,11 +2,14 @@ DROP FUNCTION deleteOrganisationUnit(VARCHAR);
 
 CREATE OR REPLACE FUNCTION deleteOrganisationUnit(orgunit VARCHAR) RETURNS VOID AS $$
 DECLARE
-	organsationUnit RECORD;
+	orgUnit_regex VARCHAR;
 
 BEGIN
 	RAISE INFO 'orgUnitId to be delete is %',orgunit;
-	EXECUTE format('COPY((SELECT * FROM organisationunit WHERE uid = %L)) TO %L WITH CSV HEADER;',orgunit,CONCAT(CONCAT('/home/chingalo/development/scripts/organisation-unit-app/data/orgunit_',orgunit),'.csv'));	
+	orgUnit_regex := CONCAT(CONCAT('%',orgunit),'%');
+	RAISE INFO 'orgUnit_regex to be delete is %',orgUnit_regex;
+	
+	EXECUTE format('COPY((SELECT * FROM organisationunit WHERE path ILIKE %L)) TO %L WITH CSV HEADER;',orgUnit_regex,CONCAT(CONCAT('/home/chingalo/development/scripts/organisation-unit-app/data/orgunit_',orgunit),'.csv'));	
 END;
 $$
 LANGUAGE plpgsql;
